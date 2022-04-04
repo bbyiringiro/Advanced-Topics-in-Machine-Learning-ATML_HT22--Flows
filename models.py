98736f7fea3af1d6b9b298b5e7d5b6d553651645
David Khachaturov <d77777k@gmail.com>
2022-04-04 17:00:13 +0100

    Add configurable non-linearity layer to model

--- models.py
+++ models.py
@@ -1,14 +1,101 @@
 import torch
 import torch.nn as nn
 from flows import *
+from torch.autograd import Variable
+import math
 
 from typing import List
 
+
+class Encoder(nn.Module):
+    def __init__(self, dim_input: int, hidden_dims: List[int],
+                non_linearity: str = "MaxOut",
+                latent_size: int =40, maxout_window_size: int =4):
+        super(Encoder, self).__init__()
+        assert(len(hidden_dims) >= 1)
+
+        self.maxout_window_size=maxout_window_size
+        self.latent_size = latent_size
+        hidden_dims = [dim_input] + hidden_dims
+        
+        self.layers = []
+        if non_linearity == "MaxOut":
+          first = True
+          for i in range(len(hidden_dims) - 1):
+            if first:
+              self.layers.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
+              self.layers.append(nn.MaxPool1d(kernel_size=self.maxout_window_size, stride=self.maxout_window_size))
+              first = False
+            else:
+              self.layers.append(nn.Linear(hidden_dims[i]//(self.maxout_window_size), hidden_dims[i+1]))
+              self.layers.append(nn.MaxPool1d(kernel_size=self.maxout_window_size, stride=self.maxout_window_size))
+
+          self.fc_mean = nn.Linear(hidden_dims[-1]//(self.maxout_window_size), self.latent_size)
+          self.fc_var = nn.Linear(hidden_dims[-1]//(self.maxout_window_size), self.latent_size)
+
+        elif non_linearity == "ReLU":
+            for i in range(len(hidden_dims) - 1):
+              self.layers.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
+              self.layers.append(nn.ReLU)
+
+            self.fc_mean = nn.Linear(hidden_dims[-1], self.latent_size)
+            self.fc_var = nn.Linear(hidden_dims[-1], self.latent_size)
+        else:
+          raise ModuleNotFoundError(f"Error: {non_linearity} is not a valid non linearity layer.")
+
+        self.seqNet = nn.Sequential(*self.layers)
+
+    def forward(self, x):
+        h = self.seqNet(x)
+        
+        return self.fc_mean(h), self.fc_var(h), h
+
+
+class Decoder(nn.Module):
+    def __init__(self, dim_input: int, out_dim: int, hidden_dims: List[int],
+                non_linearity: str = "MaxOut", maxout_window_size=4):
+        super(Decoder, self).__init__()
+
+        assert(len(hidden_dims) >= 1)
+
+        self.maxout_window_size=maxout_window_size
+        
+        hidden_dims = [dim_input] + hidden_dims
+
+        self.layers = []
+        if non_linearity == "MaxOut":
+          first = True;
+          for i in range(len(hidden_dims) - 1):
+            if first:
+              self.layers.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
+              self.layers.append(nn.MaxPool1d(kernel_size=maxout_window_size, stride=self.maxout_window_size))
+              first = False
+            else:
+              self.layers.append(nn.Linear(hidden_dims[i]//(self.maxout_window_size), hidden_dims[i+1]))
+              self.layers.append(nn.MaxPool1d(kernel_size=self.maxout_window_size, stride=self.maxout_window_size))
+          
+          self.layers.append(nn.Linear(hidden_dims[-1]//self.maxout_window_size, out_dim))
+
+        elif non_linearity == "ReLU":
+          for i in range(len(hidden_dims) - 1):
+            self.layers.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
+          self.layers.append(nn.Linear(hidden_dims[-1], out_dim))
+
+        else:
+          raise ModuleNotFoundError(f"Error: {non_linearity} is not a valid non linearity layer.")
+        
+        self.seqNet = nn.Sequential(*self.layers)
+
+    def forward(self, x):
+        return torch.sigmoid(self.seqNet(x))
+
 class FlowModule(nn.Module):
-  def __init__(self, dim_input:int, num_layers: int, flow_type:str ='Planar'):
+  def __init__(self, dim_input:int, num_layers: int,
+              encoder_out_dim = None, flow_type:str ='Planar'):
     super(FlowModule, self).__init__()
     
     flow_block_class = None
+    self.flow_type = flow_type
     try:
       flow_block_class = globals()[flow_type]
     except (KeyError):
@@ -20,99 +107,131 @@ class FlowModule(nn.Module):
     elif (flow_type=='Radial'):
       Radial=flow_block_class
       self.flows = nn.ModuleList([Radial(dim_input,) for _ in range(num_layers)])
+    elif(flow_type=="TriangularSylvester"):
+
+      assert(encoder_out_dim!=None)
+      
+      self.flows = nn.ModuleList([TriangularSylvester(dim_input,) for _ in range(num_layers)])
+
+      # the following for TriangularSylvester is adapted from the orginal paper publication from:
+      #https://github.com/riannevdberg/sylvester-flows
+      # self.flip_idx
+      self.z_size =dim_input;
+      self.num_flows = num_layers
+      self.q_z_nn_output_dim = encoder_out_dim;
+
+
+       # permuting indices corresponding to Q=P (permutation matrix) for every other flow
+      flip_idx = torch.arange(self.z_size - 1, -1, -1).long()
+      self.register_buffer('flip_idx', flip_idx)
+
+      # Masks needed for triangular r1 and r2.
+      triu_mask = torch.triu(torch.ones(self.z_size, self.z_size), diagonal=1)
+      triu_mask = triu_mask.unsqueeze(0).unsqueeze(3)
+      diag_idx = torch.arange(0, self.z_size).long()
+
+      self.register_buffer('triu_mask', Variable(triu_mask))
+      self.triu_mask.requires_grad = False
+      self.register_buffer('diag_idx', diag_idx)
+
+      # Amortized flow parameters
+      # Diagonal elements of r1 * r2 have to satisfy -1 < r1 * r2 for flow to be invertible
+      self.diag_activation = nn.Tanh()
+
+      self.amor_d = nn.Linear(self.q_z_nn_output_dim, self.num_flows * self.z_size * self.z_size)
+
+      self.amor_diag1 = nn.Sequential(
+          nn.Linear(self.q_z_nn_output_dim, self.num_flows * self.z_size),
+          self.diag_activation
+      )
+      self.amor_diag2 = nn.Sequential(
+          nn.Linear(self.q_z_nn_output_dim, self.num_flows * self.z_size),
+          self.diag_activation
+      )
+
+      self.amor_b = nn.Linear(self.q_z_nn_output_dim, self.num_flows * self.z_size)
+
+
+
     else:
       raise NotImplementedError
 
-  def forward(self, zo):
+  def sylvgetMortisedParameters(self, h):
+    batch_size =h.size(0) # TASK
+    h = h.view(-1, self.q_z_nn_output_dim)
+
+    # Amortized r1, r2, q, b for all flows
+
+
+    full_d = self.amor_d(h)
+    diag1 = self.amor_diag1(h)
+    diag2 = self.amor_diag2(h)
+
+    
+    full_d = full_d.resize(batch_size, self.z_size, self.z_size, self.num_flows)
+    diag1 = diag1.resize(batch_size, self.z_size, self.num_flows)
+    diag2 = diag2.resize(batch_size, self.z_size, self.num_flows)
+
+    r1 = full_d * self.triu_mask
+    r2 = full_d.transpose(2, 1) * self.triu_mask
+
+    r1[:, self.diag_idx, self.diag_idx, :] = diag1
+    r2[:, self.diag_idx, self.diag_idx, :] = diag2
+
+    b = self.amor_b(h)
+
+    # Resize flow parameters to divide over K flows
+    b = b.resize(batch_size, 1, self.z_size, self.num_flows)
+
+    return r1, r2, b
+
+
+  def forward(self, zo, h=None):
     log_det_sum = 0.
     zk=zo
-    for flow_block in self.flows:
-      zk, log_det = flow_block(zk)
-      log_det_sum +=log_det
+    if(self.flow_type != "TriangularSylvester"):
+      for flow_block in self.flows:
+        zk, log_det = flow_block(zk)
+        log_det_sum +=log_det
+    else:
 
+      #get amortise  r1, r2, b for all flows
+       r1, r2, b = self.sylvgetMortisedParameters(h)
+
+
+       for k, flow_block in enumerate(self.flows):
+          if k % 2 == 1:
+              permute_z = self.flip_idx
+          else:
+              permute_z = None
+
+          z_k, log_det_sum = flow_block(zk, r1[:, :, :, k], r2[:, :, :, k], b[:, :, :, k], permute_z, sum_ldj=True)
+
+          log_det_sum += log_det_sum
     return zk, log_det_sum
 
 
-class Encoder(nn.Module):
-    def __init__(self, dim_input: int,hidden_dims: List[int], latent_size: int =40, maxout_window_size: int =4):
-        super(Encoder, self).__init__()
-        assert(len(hidden_dims) >= 1)
-
-        self.maxout_window_size=maxout_window_size
-        self.latent_size = latent_size
-        hidden_dims = [dim_input] + hidden_dims
-        
-        self.layers = []
-
-        first = True;
-        
-        for i in range(len(hidden_dims) - 1):
-          if first:
-            self.layers.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
-            self.layers.append(nn.MaxPool1d(kernel_size=self.maxout_window_size, stride=self.maxout_window_size))
-            first = False
-          else:
-            self.layers.append(nn.Linear(hidden_dims[i]//(self.maxout_window_size), hidden_dims[i+1]))
-            self.layers.append(nn.MaxPool1d(kernel_size=self.maxout_window_size, stride=self.maxout_window_size))
-
-
-        self.seqNet = nn.Sequential(*self.layers)
-
-        self.fc_mean = nn.Linear(hidden_dims[-1]//(self.maxout_window_size), self.latent_size)
-        self.fc_var = nn.Linear(hidden_dims[-1]//(self.maxout_window_size), self.latent_size)
-
-    def forward(self, x):
-        z = self.seqNet(x)
-        
-        return self.fc_mean(z), self.fc_var(z)
-
-
-class Decoder(nn.Module):
-    def __init__(self, dim_input: int, out_dim: int, hidden_dims: List[int], maxout_window_size=4):
-        super(Decoder, self).__init__()
-
-        assert(len(hidden_dims) >= 1)
-
-        self.maxout_window_size=maxout_window_size
-        
-        hidden_dims = [dim_input] + hidden_dims
-
-        
-        self.layers = []
-        first = True;
-
-        for i in range(len(hidden_dims) - 1):
-          if first:
-            self.layers.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
-            self.layers.append(nn.MaxPool1d(kernel_size=maxout_window_size, stride=self.maxout_window_size))
-            first = False
-          else:
-            self.layers.append(nn.Linear(hidden_dims[i]//(self.maxout_window_size), hidden_dims[i+1]))
-            self.layers.append(nn.MaxPool1d(kernel_size=self.maxout_window_size, stride=self.maxout_window_size))
-
-        
-        self.layers.append(nn.Linear(hidden_dims[-1]//self.maxout_window_size, out_dim))
-        
-        self.seqNet = nn.Sequential(*self.layers)
-
-        
-    def forward(self, x):
-        return torch.sigmoid(self.seqNet(x))
-
-
-class NormalisingFlowModel(nn.Module):
+class NormalisingFlowModelVAE(nn.Module):
   def __init__(self,
                dim_input: int,
                e_hidden_dims: List[int],
                d_hidden_dims:List[int],
                flow_layers_num: int,
                flow_type: str,
-               act=torch.tanh, latent_size =40, maxout_window_size=4):
-      super().__init__()
-
-      self.encoder = Encoder(dim_input, e_hidden_dims, latent_size = latent_size, maxout_window_size=maxout_window_size)
-      self.flow = FlowModule(latent_size, num_layers=flow_layers_num, flow_type=flow_type)
-      self.decoder = Decoder(latent_size,dim_input, d_hidden_dims)
+               non_linearity: str = "MaxOut",
+               latent_size=40, maxout_window_size=4, encoder_out_dim=400):
+    super().__init__()
+    self.flow_type = flow_type
+    self.encoder = Encoder(dim_input, e_hidden_dims,
+                          non_linearity = non_linearity,
+                          latent_size = latent_size,
+                          maxout_window_size = maxout_window_size)
+    self.flow = FlowModule(latent_size, num_layers = flow_layers_num,
+                          flow_type = flow_type,
+                          encoder_out_dim = encoder_out_dim)
+    self.decoder = Decoder(latent_size, dim_input, d_hidden_dims,
+                          non_linearity = non_linearity,
+                          maxout_window_size = maxout_window_size)
 
   def reparameterize(self, mu, logvar):
       # returns a random sample from the approximate posterior q(z|x) 
@@ -130,12 +249,17 @@ class NormalisingFlowModel(nn.Module):
 
     #Inference Network
     #Encode
-    mu, log_var = self.encoder(z)
+    mu, log_var, h = self.encoder(z)
     # Reparameterise to sample z_o
+
     z_o = self.reparameterize(mu, log_var)
 
     # pass zo through the flows to get zk
-    z_k, log_det_sum = self.flow(z_o)
+
+    if(self.flow_type == "TriangularSylvester"):
+      z_k, log_det_sum = self.flow(z_o, h=h)
+    else:
+      z_k, log_det_sum = self.flow(z_o)
 
     #Generative Model
     #decode x_hat
@@ -151,4 +275,5 @@ class NormalisingFlowModel(nn.Module):
 
 
     return recon, logp_zo, logp_zk, log_det_sum
+
 
